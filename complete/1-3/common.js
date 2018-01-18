7069652701a74145eb87b6161be04d2e3d0cf69e
tommy <tooto1985@gmail.com>
2018-01-18 23:24:06 +0800

    native 1

--- complete/1-3/common.js
+++ complete/1-3/common.js
@@ -1,12 +1,15 @@
-﻿$(function() {
-    $(".menu").load("Menu.html", function() {
-        $("a").each(function() {
-          if(this.pathname == location.pathname) {
-              $(this).addClass("selected");
-          }
-        });
-        if ($("a").is(".selected") == false) {
-            $("a").first().addClass("selected");
+﻿fetch("Menu.html").then(function (response) {
+    return response.text();
+}).then(function (text) {
+    var isSelected = false;
+    document.querySelector(".menu").innerHTML = text;
+    document.querySelectorAll("a").forEach(function (a) {
+        if (a.pathname == location.pathname) {
+            isSelected = true;
+            a.setAttribute("class", "selected");
         }
     });
+    if (isSelected == false) {
+        document.querySelectorAll("a")[0].setAttribute("class", "selected");
+    }
 });
